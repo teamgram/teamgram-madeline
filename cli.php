aacba3cde810a71be88630b1580954fe6674fd10
teamgram.io <wubenqi@gmail.com>
2022-04-14 17:46:15 +0800

    patch by @teamgram

--- cli.php
+++ cli.php
@@ -9,16 +9,16 @@ $me = $MadelineProto->getSelf();
 $MadelineProto->logger($me);
 
 if (!$me['bot']) {
-    $MadelineProto->messages->sendMessage(['peer' => '@danogentili', 'message' => "Hi!\nThanks for creating MadelineProto! <3"]);
-    $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+    $MadelineProto->messages->sendMessage(['peer' => '@benqi', 'message' => "Hi!\nThanks for creating MadelineProto! <3"]);
+    // $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+    //
+    // try {
+    //     $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
+    // } catch (\danog\MadelineProto\RPCErrorException $e) {
+    //     $MadelineProto->logger($e);
+    // }
 
-    try {
-        $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
-    } catch (\danog\MadelineProto\RPCErrorException $e) {
-        $MadelineProto->logger($e);
-    }
-
-    $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
+    // $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
 }
 $MadelineProto->echo('OK, done!');
 
